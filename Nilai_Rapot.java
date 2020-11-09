c9a877060f4bbf74cbce949d0d0ac0fa4617f333
Sitasrsw <70082171+Sitasrsw@users.noreply.github.com>
2020-11-09 22:20:32 +0700

    Add files via upload

---
+++ Nilai_Rapot.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author  Sita S
+ */
+public class Nilai_Rapot {
+     public int getnilaiPengetahuan (int pengetahuan){
+        return pengetahuan;
+    }
+
+    public int getnilaiKeterampilan (int keterampilan){
+        return keterampilan;
+    }
+    
+}
